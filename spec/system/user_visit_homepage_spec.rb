7660c5dc2efada326fa0dd9345beb559e9cbecef
Adriana Boza <adboza@gmail.com>
2022-10-27 09:22:50 -0300

    fim aula 48 restes branch e switch

--- spec/system/user_visit_homepage_spec.rb
+++ spec/system/user_visit_homepage_spec.rb
@@ -22,4 +22,40 @@ describe 'Usuário visita tela inicial'do
      #Assert
      expect(page).to have_content 'Nenhum galpão encontrado'
   end
+
+  it 'e vê detalhes de galpão' do
+    #Arrange
+    json_data = File.read(Rails.root.join('spec/support/json/warehouses.json'))
+    fake_response = double("faraday_response", status: 200, body: json_data)
+    allow(Faraday).to receive(:get).with('http://localhost:4000/api/v1/warehouses').and_return(fake_response)
+    json_data = File.read(Rails.root.join('spec/support/json/warehouse.json'))
+    fake_response = double("faraday_response", status: 200, body: json_data)
+    allow(Faraday).to receive(:get).with('http://localhost:4000/api/v1/warehouses/1').and_return(fake_response)
+
+    #Act
+    visit root_path
+    click_on 'Aeroporto SP'
+    #Assert
+    expect(page).to have_content 'Galpão GRU - Aeroporto SP'
+    expect(page).to have_content 'Guarulhos'
+    expect(page).to have_content '100000 m2'
+    expect(page).to have_content 'Avenida do Aeroporto, 1000 - CEP 15000-000'
+    expect(page).to have_content 'Galpão destinado para cargas internacionais'
+  end
+  it 'e não é possível carregar galpão' do
+    #Arrange
+    json_data = File.read(Rails.root.join('spec/support/json/warehouses.json'))
+    fake_response = double("faraday_response", status: 200, body: json_data)
+    allow(Faraday).to receive(:get).with('http://localhost:4000/api/v1/warehouses').and_return(fake_response)
+    json_data = File.read(Rails.root.join('spec/support/json/warehouse.json'))
+    fake_response = double("faraday_response", status: 500, body: "{}")
+    allow(Faraday).to receive(:get).with('http://localhost:4000/api/v1/warehouses/1').and_return(fake_response)
+
+    #Act
+    visit root_path
+    click_on 'Aeroporto SP'
+    #Assert
+    expect(current_path).to eq root_path
+    expect(page).to have_content 'Não foi possível carregar galpão'    
+  end
 end
